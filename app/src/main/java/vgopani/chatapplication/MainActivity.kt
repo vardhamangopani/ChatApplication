f1be5d2b9586fee3d088a7730a20750be2dd8b1e
vardhamangopani <vardhamangopani@gmail.com>
2017-12-09 20:26:20 -0800

    New Project2

--- app/src/main/java/vgopani/chatapplication/MainActivity.kt
+++ app/src/main/java/vgopani/chatapplication/MainActivity.kt
@@ -28,6 +28,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
+        //hiiiiii
         return true
     }
 
